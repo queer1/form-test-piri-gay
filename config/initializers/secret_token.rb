95e6a3e3d7579ea440f273abf58671c37f0de797
Juan Pablo Giordano <jpgiordanolorca@gmail.com>
2013-12-24 10:50:29 -0300

    piri gay

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InfoWrist::Application.config.secret_token = 'b78e09626c749428818439173043594eb53a990d46ed97ec46502616e34efd482d52e800650a652a21ad7b6ee2ebb26513ad1d34cec8b68cd3551ef12f0ae9fb'
